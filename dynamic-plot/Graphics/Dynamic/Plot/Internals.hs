da1e5cb05417c3108962dab1c45d6b615bd35a89
Justus Sagemüller <jsag@hvl.no>
2023-05-14 16:04:51 +0200

    Forgot to add module re-exporting the internals.

---
+++ dynamic-plot/Graphics/Dynamic/Plot/Internals.hs
@@ -0,0 +1,24 @@
+-- |
+-- Module      : Graphics.Dynamic.Plot.Internals
+-- Copyright   : (c) Justus Sagemüller 2023
+-- License     : GPL v3
+-- 
+-- Maintainer  : (@) jsag $ hvl.no
+-- Stability   : experimental
+-- Portability : requires GHC>6 extensions
+
+
+module Graphics.Dynamic.Plot.Internals (
+          module Graphics.Dynamic.Plot.R2.Internal
+        , module Graphics.Dynamic.Plot.Internal.Types
+        , module Graphics.Dynamic.Plot.Colour
+        , module Graphics.Text.Annotation
+        , module Graphics.Image.Resample
+        ) where
+
+import Graphics.Dynamic.Plot.R2.Internal
+import Graphics.Dynamic.Plot.Internal.Types
+import Graphics.Dynamic.Plot.Colour
+import Graphics.Text.Annotation
+import Graphics.Image.Resample
+
